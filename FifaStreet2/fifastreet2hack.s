746134fe92a37fac7072135d2ca51475b526939f
Dave Murphy <davem@devkitpro.org>
2014-05-04 15:23:36 +0100

    add FIFA Street 2 exploit

---
+++ FifaStreet2/fifastreet2hack.s
@@ -0,0 +1,248 @@
+/*
+	Copyright 2014 Dave Murphy (WinterMute)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#define	REG_BASE	0x04000000
+
+	.global _start
+_start:
+	.word	0xf1fa06ee, 0x0b89, 0x7eef004d, 0
+	.word	0, 0, 0, 0
+	.word	0, 0x01010101, 0, 0
+	.word	0, 0x0b0000, 0x7f7f0100, 0x0303037f
+	.fill	5, 1, 3
+	.ascii	"hello world"
+	.fill	272, 1, 'x'
+	.fill	9, 1, 'x'
+	.byte	0xb4, 0x2b, 0x0c, 0x02
+	.fill	35, 1, 'x'
+
+hack:
+	mov	r12, #REG_BASE
+	str	r12, [r12, #0x208]		@ IME = 0;
+
+	ldr	r0, =0x00002078			@ disable TCM and protection unit
+	mcr	p15, 0, r0, c1, c0
+
+	@ Disable caches
+	mov	r0, #0
+	mcr	p15, 0, r0, c7, c5, 0		@ Instruction cache
+	mcr	p15, 0, r0, c7, c6, 0		@ Data cache
+
+	@ Wait for write buffer to empty
+	mcr	p15, 0, r0, c7, c10, 4
+
+	mov	r1, #0
+	strh	r1, [r12, #0x6c]
+
+	mov	r2, #0x1000
+	add	r3, r2, r12
+	strh	r1, [r3, #0x6c]
+
+	add	r11, r12, #0x1a0
+
+	ldrh	r0,[r11,#0x204-0x1a0]
+	and	r0,r0,#~(1<<11)
+	strh	r0,[r11,#0x204-0x1a0]
+
+	ldr	r0, eepromselect
+	strh	r0,[r11]
+
+	mov	r0,#03
+	strb	r0,[r11,#0x02]
+	bl	eepromwait
+
+	mov	r0,#0
+	strb	r0,[r11,#0x02]
+	bl	eepromwait
+
+	mov	r0,#0
+	strb	r0,[r11,#0x02]
+	bl	eepromwait
+
+	mov	r1,#0x02300000
+	add	r2,r1,#0x2000
+
+readeeprom:
+	mov	r0,#0
+	strb	r0,[r11,#0x02]
+	
+	bl	eepromwait
+	ldrb	r0,[r11,#0x02]
+	strb	r0,[r1],#1
+	cmp	r1,r2
+	bne	readeeprom
+
+	mov	r0,#0x40
+	strh	r0,[r11]
+
+	mov	r11,r11
+	ldr	r0,stage2_offset
+	add	r0,#0x02300000
+	bx	r0
+
+stage2_offset:
+	.word	stage2 - _start
+
+eepromwait:
+	ldrh	r0,[r11]
+	tst	r0, #128
+	bne	eepromwait
+	bx	lr
+
+eepromselect:
+	.word	0xA040
+
+stage2:
+	mov	r2, #0x1000
+	add	r2,r2,r12
+	mov	r0,r12
+	ldr	r3, dispcnt
+	str	r3, [r0]
+	str	r3, [r2]
+
+	mov	r5, #0x200
+	str	r5, [r0, #0x08]
+	str	r5, [r2, #0x08]
+	
+	add	r0, r0, #0x240
+	str	r1, [r0]
+	strh	r1, [r0, #0x04]
+	strb	r1, [r0, #0x06]
+	strb	r1, [r0, #0x08]
+	
+	mov	r1, #0x81
+	strb	r1, [r0]
+	mov	r1, #0x84
+	strb	r1, [r0,#0x02]
+
+	mov	r4, #0x05000000		@ engine A palette
+
+	mov	r1, #0x1f
+	str	r1, [r4]
+	str	r1, [r4,#0x400]		@ engine B palette
+
+	add	r0, r4, #0x01000000
+	add	r2, r0, #0x00200000
+
+	mov	r1, #0
+	mov	r3, #0x1800
+	bl	memset
+
+	mov	r0, #0x40000
+	add	r0, #0xC
+	str	r0, [r12, #0x188]
+
+	add	r3, r12, #0x180		@ r3 = 4000180
+
+	ldr	r5,=0x2fffc24
+
+	ldr	r0,=0x4004008
+	ldr	r0,[r0]
+	ands	r0,r0,#0x8000
+	beq	notDSi
+
+	mov	r2, #4
+	strh	r2, [r5,#4]
+	bl	wait_dsi7
+	mov	r2, #3
+	strh	r2, [r5,#4]
+	bl	wait_dsi7
+
+notDSi:
+	mov	r2, #1
+	bl	waitsync
+
+	mov	r1, #0x3e0
+	str	r1, [r4]
+
+	adr	r5, arm7_start
+	adr	r7, arm7_end
+	ldr	r6, =0x2380000
+copyloop:
+	ldr	r0, [r5],#4
+	str	r0, [r6],#4
+	cmp	r5, r7
+	bne	copyloop
+
+	mov	r0, #0x100
+	strh	r0, [r3]
+
+	mov	r2, #0
+	bl	waitsync
+
+	mov	r0, #0
+	strh	r0, [r3]
+
+	mov	r2, #5
+	bl	waitsync
+
+	str	r1, [r4,#0x400]		@ engine B palette
+
+forever:
+	b	forever
+	
+	.pool
+
+dispcnt:
+	.word	0x10100
+
+memset:
+
+.clrloop:
+	subs	r3, r3, #4
+	str	r1, [r0,r3]
+	str	r1, [r2,r3]
+	bne	.clrloop
+	bx	lr
+
+wait_dsi7:
+	ldrh	r0,[r5,#2]
+.wait7:
+	ldrh	r6,[r5,#2]
+	cmp	r6,r0
+	beq	.wait7
+
+	ldrh	r0,[r5]
+	add	r0,r0,#1
+	strh	r0,[r5]
+	bx	lr
+
+waitsync:
+	ldrh	r0, [r3]
+	and	r0, r0, #0x000f
+	cmp	r0, r2
+	bne	waitsync
+	bx	lr
+
+
+arm7_start:
+	mov	r12, #REG_BASE
+	str	r12, [r12, #0x208]	@ IME = 0;
+	add	r3, r12, #0x180
+	mov	r0,#0x0500
+	strh	r0,[r3]
+	ldr	r0,loop
+	mov	r1,#0x3800000
+	str	r0,[r1]
+	bx	r1
+loop:
+	b	loop
+
+arm7_end:
+
+	.space (_start + 0x2000) - .
---
+++ FifaStreet2/fifastreet2sum.c
@@ -0,0 +1,91 @@
+﻿#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+uint16_t checksum(unsigned char *buffer, int length) {
+
+	uint16_t sum = 13;
+	
+	do {
+
+		if (length<4) {
+
+			sum += (*(buffer++) * length);
+			length--;
+
+		} else {
+
+			sum += (*(buffer++) * length);
+			sum += (*(buffer++) * length);
+			sum += (*(buffer++) * length);
+			sum += (*(buffer++) * length);
+			
+			length -= 4;
+		}		
+	} while (length > 0 );
+	return sum;
+}
+
+unsigned char *loadFile(char *name, int *bufferSize) {
+	FILE * f;
+	long size;
+	unsigned char * buffer;
+
+	f=fopen(name , "rb" );
+	if(!f)return NULL;
+
+	fseek (f , 0 , SEEK_END);
+	size = ftell (f);
+	rewind (f);
+	if(bufferSize)*bufferSize=size;
+
+	buffer=(unsigned char*)malloc(sizeof(char)*size);
+	fread(buffer,1,size,f);
+	fclose(f);
+	return buffer;
+}
+
+void saveFile(const char filename[], unsigned char* buffer, long size)
+{
+	FILE * f;
+
+	f=fopen(filename , "wb+" );
+	if(!f)return;
+
+	fwrite(buffer,1,size,f);
+
+	fclose(f);
+}
+
+int main(int argc, char* argv[]) {
+
+	if (argc < 2) {
+		printf("Usage: %s savefile",argv[0]);
+		exit(1);
+	}
+
+	int size;
+	
+	unsigned char *buffer = loadFile(argv[1],&size);
+
+	if (buffer == NULL) {
+		printf ("failed to load %s.\n",argv[1]);
+		exit(1);
+	}
+	
+	
+	int oldsum = (buffer[11] << 8) + buffer[10];
+	int newsum = checksum(&buffer[0x34],0xfa0);
+	
+	printf("current checksum = %04x\ncalculated checksum = %04x\n", oldsum,newsum);
+
+	buffer[10] = newsum & 0xff;
+	buffer[11] = (newsum >> 8 ) & 0xff;
+
+	saveFile(argv[1],buffer,size);
+	
+	free(buffer); 
+	return 0;
+}
+
